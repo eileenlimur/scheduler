1ab3dc3f390b44ce4f88b46aba393baf2cb939cc
Eileen Li <eileenlimur@gmail.com>
2020-07-07 18:08:27 -0400

    second commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,21 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classnames from 'classnames';
+
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classnames("button", {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   })
+   
+   return (
+      <button
+         className={buttonClass}
+         disabled={props.disabled}
+         onClick={props.onClick}
+      >
+         {props.children}
+      </button>
+   );
 }
---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classnames from 'classnames';
+import DayListItem from './DayListItem';
+
+export default function DayList(props) {
+  const DayListLoop = props.days.map(day => 
+    (
+  <DayListItem
+  key={day.id}
+  name={day.name}
+  spots={day.spots}
+  selected={day.name === props.day}
+  setDay={props.setDay}
+  />
+  ))
+  
+  return <ul>{DayListLoop}</ul>
+}
+
+//three props: days(array), day(string), setDay(function)
+//no styles of its own
+//renders list of DayListItems
