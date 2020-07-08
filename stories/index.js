1993c52ff001d96c0fc7e48592e29e846a9f95eb
Eileen Li <eileenlimur@gmail.com>
2020-07-08 02:06:51 -0400

    complete Show component and Show story

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf("Button", module)
 .addParameters({
@@ -153,4 +154,11 @@ storiesOf('Appointment', module)
   ))
   .add('Empty', () => (
     <Empty onAdd={action('onAdd')}/>
+  ))
+  .add('Show', () => (
+    <Show
+    student='Lydia Miller-Jones'
+    interviewer={interviewer}
+    onEdit={action('Edit')}
+    onDelete={action('Delete')}/>
   ))
